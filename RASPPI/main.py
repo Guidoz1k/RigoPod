6281cd782730377a7570ed24f6eab2186ff561cb
Rigo <rodrigo.embsys@gmail.com>
2026-03-04 22:25:37 -0300

    we have opencv working

--- RASPPI/main.py
+++ RASPPI/main.py
@@ -1,1 +1,40 @@
-print("hello my wiggas")
+from picamera2 import Picamera2
+import cv2
+import numpy as np
+
+picam2 = Picamera2()
+picam2.configure( picam2.create_preview_configuration() )
+picam2.start()
+
+while True:
+    frame = picam2.capture_array()
+    frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    # Example: detect orange object
+    lower = np.array([100, 150, 50])
+    upper = np.array([130, 255, 255])
+    mask = cv2.inRange(hsv, lower, upper)
+    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    if contours:
+        c = max(contours, key=cv2.contourArea)
+
+        x, y, w, h = cv2.boundingRect(c)
+
+        cx = x + w // 2
+        cy = y + h // 2
+
+        # draw detection
+        cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 2)
+        cv2.circle(frame, (cx,cy), 5, (0,0,255), -1)
+
+        print("Center:", cx, cy)
+
+    cv2.imshow("Camera", frame)
+    cv2.imshow("Mask", mask)
+    if cv2.waitKey( 1 ) == ord( 'q' ):
+        break
+
+cv2.destroyAllWindows()
+picam2.stop()
